1b6401475a05d2c4b3f672ec2b07c0f57553ff83
sayma63 <saymasumi106353@gmail.com>
2023-02-09 15:32:39 +0600

    order api added

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors=require('cors');
 const app= express();
 const port=process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+// const { ObjectID } = require('bson');
 //middle wares
 app.use(cors());
 app.use(express.json());
@@ -14,11 +15,52 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cnkfgua.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+
+    try{
+        const productCollection=client.db('fashion_gallery').collection('products');
+        const orderCollection= client.db('fashion_gallery').collection('orders')
+        
+
+
+
+
+
+
+
+
+
+
+           //All products api
+        app.get('/products', async(req,res)=>{
+            const query={}
+            const cursor=productCollection.find(query);
+            const products= await cursor.toArray();
+            res.send(products);
+           });
+         //single product details api
+         app.get('/products/:id', async(req,res) => {
+            const id = req.params.id;
+            
+            const query = {_id: new ObjectId(id)};
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
+
+        //orders api
+        app.post('/orders', async (req,res)=>{
+            const order =req.body;
+            const result= await orderCollection.insertOne(order)
+            res.send(result);
+        })
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(error=>console.error(err))
 
 
 app.get('/',(req,res)=>{
