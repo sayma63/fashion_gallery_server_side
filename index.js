e89b4b6bf1a408da737f1b6c90a80442de727061
sayma63 <saymasumi106353@gmail.com>
2023-02-11 18:08:33 +0600

    make admin api added

--- index.js
+++ index.js
@@ -106,6 +106,25 @@ async function run(){
                 const query= {};
                 const users= await usersCollection.find(query).toArray();
                 res.send(users);
+            });
+            //make admin api
+            app.put('/users/admin/:id', verifyJWT, async(req,res)=>{
+                const decodedEmail= req.decoded.email;
+                const query= {email: decodedEmail}
+                const user= await usersCollection.findOne(query);
+                if(user?.role!=='admin'){
+                    return res.status(403).send({message:'forbidden access'})
+                }
+                const id = req.params.id;
+                const filter=  {_id: new ObjectId(id)};
+                const options= {upsert : true};
+                const updatedDoc ={
+                    $set:{
+                        role:'admin'
+                    }
+                }
+                const result= await usersCollection.updateOne(filter,updatedDoc,options);
+                res.send(result)
             })
 
             
